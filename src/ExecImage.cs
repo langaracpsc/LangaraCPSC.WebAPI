83f29a5e419cf137af441ff0acccd003e4dbf934
rishit-singh <rsrishitsingh@gmail.com>
2023-06-17 18:21:07 -0700

    Added required subroutines to ExecImageManager

--- src/ExecImage.cs
+++ src/ExecImage.cs
@@ -66,6 +66,36 @@ public class ExecImageManager
         return this.DatabaseInstance.InsertRecord(execImage.ToRecord(), this.ExecImageTable.Name);
     }
 
+    public bool ExecImageExists(string id)
+    {
+        return (this.DatabaseInstance.FetchQueryData($"SELECT * FROM {this.ExecImageTable.Name} WHERE ID=\'{id}\'", this.ExecImageTable.Name).Length > 0);
+    }
+
+    public ExecImageBase64 GetImageByID(string id)
+    {
+        if (this.ExecImageMap.ContainsKey(id))
+            return this.ExecImageMap[id];
+        
+        ExecImageBase64 image = null;
+
+        Record[] records;
+
+        if ((records = this.DatabaseInstance.FetchQueryData($"SELECT * FROM {this.ExecImageTable.Name} WHERE ID=\'{id}\'", this.ExecImageTable.Name)) == null)
+            return null; 
+        
+        this.ExecImageMap.Add((image = ExecImageBase64.LoadFromFile(records[0].Values[1].ToString())).ID, image);
+
+        return image;
+    }
+
+    public bool DeleteImage(string id)
+    {
+        if (this.ExecImageExists(id))
+            return this.DatabaseInstance.ExecuteQuery($"DELETE FROM {this.ExecImageTable.Name}");
+
+        return false;
+    }
+    
     public ExecImageManager(DatabaseConfiguration configuration, string tableName = "ExecImages")
     {
         this.DatabaseInstance = new PostGRESDatabase(configuration);
