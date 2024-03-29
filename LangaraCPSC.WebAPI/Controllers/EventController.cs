a01601469ef2ca48856843e9754cbbfb68c63a11
rishit-singh <rsrishitsingh@gmail.com>
2024-03-29 16:52:53 -0700

    Added output caching to EventController

--- LangaraCPSC.WebAPI/Controllers/EventController.cs
+++ LangaraCPSC.WebAPI/Controllers/EventController.cs
@@ -1,25 +1,39 @@
 using KeyMan;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OutputCaching;
 
 namespace LangaraCPSC.WebAPI.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class EventController
+    public class EventController : ControllerBase
     {
         private readonly APIKeyManager _ApiKeyManager;
 
         private readonly EventManager _EventManager;
         
         [HttpGet("ListAll")]
-        public async Task<string> Get([FromHeader] string apikey)
+        [OutputCache(Duration = 60)]
+        public async Task<ActionResult<string>> Get([FromHeader] string apikey)
         {
             if (!this._ApiKeyManager.IsValid(apikey, new string[]{ "ExecRead" }))
-                return new HttpError(HttpErrorType.Forbidden, "500: Forbidden").ToJson();  
+                return Forbid(new HttpError(HttpErrorType.Forbidden, "Forbidden").ToJson());  
          
-            return await Task.Run(() => new HttpObject(HttpReturnType.Success, this._EventManager.GetEvents()).ToJson());
+            return await Task.Run(() => Ok(new HttpObject(HttpReturnType.Success, this._EventManager.GetEvents()).ToJson()));
         }
+        
+        [HttpGet("{year}/{max}")]
+        [OutputCache(Duration = 60)]
+        public async Task<ActionResult<string>> GetEventsMax([FromHeader] string apikey, [FromRoute] int year, [FromRoute] int max)
+        {
+            if (!this._ApiKeyManager.IsValid(apikey, new string[]{ "ExecRead" }))
+                return Forbid(new HttpError(HttpErrorType.Forbidden, "Forbidden").ToJson());  
+            
+            List<Event> events = this._EventManager.GetEvents().Where(e => e.Start != null && DateTime.Parse(e.Start).Year == year).ToList();  
 
+            return await Task.Run(() => Ok(new HttpObject(HttpReturnType.Success, (events.Count > 0) ? events.Slice(0, events.Count >= max ? max : events.Count) : events).ToJson()));
+        }
+        
         [HttpGet("Calendar")]
         public async Task<ActionResult<string>> GetCalendarInviteLink()
         {
